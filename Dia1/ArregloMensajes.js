139e47ddbc73a3c0e6812acf8cc96c9462003d6f
Alberto Luebbert <albertoluebbert@ideashappy.com>
2016-11-30 23:11:16 -0600

    Final dia 3

---
+++ Dia1/ArregloMensajes.js
@@ -0,0 +1,389 @@
+[{
+	"canal" : "1",
+	"NombreTipoNotificacion" : "ALTA",
+	"Status" : "2",
+	"IdTipoNotificacion" : "3",
+	"nombreCanal" : "Personal",
+	"fecha" : "11-22-2016 13:47:55",
+	"fechaActualizacion" : "11-22-2016 13:47:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-12315",
+	"tituloMensaje" : "Entrega de Celulares HITSS ANS",
+	"remitente" : "Juan Ramón García",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Luis Enrique Catalina",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123154",
+		"body" : "Ramón ya tengo mi teléfono :D mucha gracias",
+		"from" : "usuarioDestinatario",
+		"created" : "2016-11-22T13:50:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123153",
+		"body" : "De que, saludos",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-22T13:49:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123152",
+		"body" : "Hola Ramón. gracias!!. Saludos",
+		"from" : "usuarioDestinatario",
+		"created" : "2016-11-22T13:48:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123151",
+		"body" : "Que tal Luis. Se te ha asignado un equipo para ANS Móvil. Por favor acercate con Ilse Karina para efecto de que te entregue tu equipo. Saludos",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-22T13:47:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "3",
+	"NombreTipoNotificacion" : "URGENTE",
+	"Status" : "2",
+	"IdTipoNotificacion" : "3",
+	"nombreCanal" : "HITSS-ANS",
+	"fecha" : "11-21-2016 13:45:55",
+	"fechaActualizacion" : "11-21-2016 13:45:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-12314",
+	"tituloMensaje" : "Vacaciones Periodo Diciembre",
+	"remitente" : "Capital Humano",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Dalia Carmona Martinez",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123142",
+		"body" : "Ok. Gracias!",
+		"from" : "usuarioDestinatario",
+		"created" : "2016-11-21T13:46:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123141",
+		"body" : "Se les avisa que para las vacaciones de diciembre deberan apegarse a las reglas del Cliente si tienen asignación o consultar con su Jefé directo inmediato.",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-21T13:45:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "1",
+	"NombreTipoNotificacion" : "MEDIA",
+	"Status" : "2",
+	"IdTipoNotificacion" : "2",
+	"nombreCanal" : "Personal",
+	"fecha" : "11-20-2016 13:43:55",
+	"fechaActualizacion" : "11-20-2016 13:43:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-12313",
+	"tituloMensaje" : "Resultados TimeOut's",
+	"remitente" : "Juan Ramón García",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Luis Enrique Catalina",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123132",
+		"body" : "Hola Ramón. gracias!!. Saludos",
+		"from" : "usuarioDestinatario",
+		"created" : "2016-11-20T13:44:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123131",
+		"body" : "Que tal Luis. La sección de Timeouts quedo excelente, felicidades. Saludos",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-20T13:43:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "2",
+	"NombreTipoNotificacion" : "URGENTE",
+	"Status" : "2",
+	"IdTipoNotificacion" : "3",
+	"nombreCanal" : "Equipo",
+	"fecha" : "11-19-2016 13:40:55",
+	"fechaActualizacion" : "11-19-2016 13:40:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-12312",
+	"tituloMensaje" : "Junta Web Entrega ",
+	"remitente" : "Araceli Bautista Cruz",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Cristhian Castañeda",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123123",
+		"body" : "A las 12 en el Piso 6",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-19T13:42:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123122",
+		"body" : "Ok, preparo todo para la presentación. ¿A qué hora?",
+		"from" : "usuarioDestinatario",
+		"created" : "2016-11-19T13:41:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123121",
+		"body" : "Cristhin, hoy presentamos la Web Urgente!",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-19T13:40:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "2",
+	"NombreTipoNotificacion" : "URGENTE",
+	"Status" : "2",
+	"IdTipoNotificacion" : "3",
+	"nombreCanal" : "Equipo",
+	"fecha" : "11-18-2016 13:37:55",
+	"fechaActualizacion" : "11-18-2016 13:37:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-12311",
+	"tituloMensaje" : "Junta App Móvil ",
+	"remitente" : "Araceli Bautista Cruz",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Alberto Luebbert M.",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123113",
+		"body" : "A las 12 en el Piso 14",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-18T13:39:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123112",
+		"body" : "Ok, preparo todo para la presentación. ¿A qué hora?",
+		"from" : "usuarioDestinatario",
+		"created" : "2016-11-18T13:38:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123111",
+		"body" : "Alberto, hoy presentamos la App Móvil Urgente!",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-18T13:37:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "2",
+	"NombreTipoNotificacion" : "URGENTE",
+	"Status" : "2",
+	"IdTipoNotificacion" : "3",
+	"nombreCanal" : "Equipo",
+	"fecha" : "11-17-2016 13:35:55",
+	"fechaActualizacion" : "11-17-2016 13:35:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-12310",
+	"tituloMensaje" : "Revisión App Móvil",
+	"remitente" : "Araceli Bautista Cruz",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Martha Guadalupe Centeno Gutiérrez",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123102",
+		"body" : "Que tal Ya lo mande desde el viernes pasado. Saludos",
+		"from" : "usuarioDestinatario",
+		"created" : "2016-11-17T13:36:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-123101",
+		"body" : "Martha, me puedes enviar toda la información al respecto de la App Móvil Urgente!",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-17T13:35:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "2",
+	"NombreTipoNotificacion" : "URGENTE",
+	"Status" : "2",
+	"IdTipoNotificacion" : "2",
+	"nombreCanal" : "Equipo",
+	"fecha" : "11-16-2016 13:33:55",
+	"fechaActualizacion" : "11-16-2016 13:33:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-1239",
+	"tituloMensaje" : "Entrega Documentación BNET Web",
+	"remitente" : "Araceli Bautista Cruz",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Martha Guadalupe Centeno Gutiérrez",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12392",
+		"body" : "Que tal Ya lo mande desde el viernes pasado. Saludos",
+		"from" : "usuarioDestinatario",
+		"created" : "2016-11-16T13:34:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12391",
+		"body" : "Martha, me puedes enviar toda la información al respecto de la Web! Urgente!",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-16T13:33:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "3",
+	"NombreTipoNotificacion" : "URGENTE",
+	"Status" : "2",
+	"IdTipoNotificacion" : "3",
+	"nombreCanal" : "HITSS-ANS",
+	"fecha" : "11-15-2016 13:32:55",
+	"fechaActualizacion" : "11-15-2016 13:32:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-1238",
+	"tituloMensaje" : "Registro Horas Diciembre",
+	"remitente" : "Capital Humano",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Dalia Carmona Martinez",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12381",
+		"body" : "Registro de Horas.",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-15T13:32:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "3",
+	"NombreTipoNotificacion" : "URGENTE",
+	"Status" : "2",
+	"IdTipoNotificacion" : "3",
+	"nombreCanal" : "HITSS-ANS",
+	"fecha" : "11-14-2016 13:31:55",
+	"fechaActualizacion" : "11-14-2016 13:31:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-1237",
+	"tituloMensaje" : "Preparativos Save the date 9  Diciembre",
+	"remitente" : "Capital Humano",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Dalia Carmona Martinez",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12371",
+		"body" : "Ya tenemos Sedes, esten pendientes.",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-14T13:31:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "3",
+	"NombreTipoNotificacion" : "URGENTE",
+	"Status" : "2",
+	"IdTipoNotificacion" : "3",
+	"nombreCanal" : "HITSS-ANS",
+	"fecha" : "11-13-2016 13:30:45",
+	"fechaActualizacion" : "11-13-2016 13:30:45",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-1236",
+	"tituloMensaje" : "Save the Date",
+	"remitente" : "Capital Humano",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Dalia Carmona Martinez",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12361",
+		"body" : "Hola a todos. El próximo 9 de Diciembre sera la fiesta navideña de HITSS. estan todos invitados, pronto enviaremos mayor información. Saludos",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-13T13:30:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "1",
+	"NombreTipoNotificacion" : "URGENTE",
+	"Status" : "2",
+	"IdTipoNotificacion" : "1",
+	"nombreCanal" : "Personal",
+	"fecha" : "11-12-2016 13:27:55",
+	"fechaActualizacion" : "11-12-2016 13:27:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-1235",
+	"tituloMensaje" : "Revisión servidor mexicoNetNI2 Canal BCOM-BNET",
+	"remitente" : "Rodrigo Aguilar Baltazar",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Cristhian Castañeda Rivera",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12353",
+		"body" : "Hola Cristhian, Muchas Gracias.",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-12T13:29:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12352",
+		"body" : "Hola Rodrigo. Ya lo revise, el disco duro se quedo sin espacio, esta funcionando en este momento correctamente. Saludos",
+		"from" : "usuarioDestinatario",
+		"created" : "2016-11-12T13:28:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12351",
+		"body" : "Hola Cristhian. Puedes revisar por favor porque el servidor MexicoNETNI2 dejo de funcionar desde las 3:30 AM. Gracias",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-12T13:27:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "2",
+	"NombreTipoNotificacion" : "URGENTE",
+	"Status" : "2",
+	"IdTipoNotificacion" : "2",
+	"nombreCanal" : "Equipo",
+	"fecha" : "11-11-2016 13:25:55",
+	"fechaActualizacion" : "11-11-2016 13:25:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-1234",
+	"tituloMensaje" : "Entrega Documentación BMóvil Alertas",
+	"remitente" : "Isaias Martinez Cruz",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Alberto Tadeo Luebbert Mendoza",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12342",
+		"body" : "Que tal Isaias, Ya se encuentra todo disponible en Google Drive para que lo puedan descargar. Saludos",
+		"from" : "usuarioDestinatario",
+		"created" : "2016-11-11T13:26:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12341",
+		"body" : "Alberto, Me solicito Ramón que se entregue toda la documentación respecto al proyecto de Bmóvil Alertas. Me puedes apoyar? Mil gracias!",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-11T13:25:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "3",
+	"NombreTipoNotificacion" : "URGENTE",
+	"Status" : "2",
+	"IdTipoNotificacion" : "3",
+	"nombreCanal" : "HITSS-ANS",
+	"fecha" : "11-10-2016 13:24:55",
+	"fechaActualizacion" : "11-10-2016 13:24:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-1233",
+	"tituloMensaje" : "Registro de horas en HOST",
+	"remitente" : "Octavio Ramirez Sifuentes",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Dalia Carmona Martinez",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12331",
+		"body" : "Hola a todos. Se solicita registren sus horas en HOST respecto a los proyectos que llevamos de ANS. Saludos",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-10T13:24:55.585Z"
+	}],
+	"responder" : '0'
+}, {
+	"canal" : "1",
+	"NombreTipoNotificacion" : "MEDIA",
+	"Status" : "2",
+	"IdTipoNotificacion" : "2",
+	"nombreCanal" : "Personal",
+	"fecha" : "11-09-2016 13:21:55",
+	"fechaActualizacion" : "11-09-2016 13:21:55",
+	"leido" : "0",
+	"id" : "68c1a793-bbac-4697-9fc6-25622a293-1232",
+	"tituloMensaje" : "Entrega Timeouts BuenFin",
+	"remitente" : "Juan Ramón García",
+	"usuarioRemitente" : "c2fa1ac1-e151-4688-8113-d4cac0cd8936",
+	"destinatario" : "Luis Enrique Catalina",
+	"usuarioDestinatario" : "c2fa1ac1-e151-4688-8113-d4cac0cd9230",
+	"mensajes" : [{
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12323",
+		"body" : "Enterado, le escribo a Cristhian. Saludos y buen fin de semana.",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-09T13:23:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12322",
+		"body" : "Hola Ramón. Cristhian se ha estado encargando de ello durante los dias que asistio a Reforma. Saludos",
+		"from" : "usuarioDestinatario",
+		"created" : "2016-11-09T13:22:55.585Z"
+	}, {
+		"id" : "68c1a793-bbac-4697-9fc6-25622a293-12321",
+		"body" : "Que tal Luis. Este fin de semana es el buen fin. La app y el Web móvil deberan ya incluir la sección Timeouts. Saludos",
+		"from" : "usuarioRemitente",
+		"created" : "2016-11-09T13:21:55.585Z"
+	}],
+	"responder" : '0'
+}];
